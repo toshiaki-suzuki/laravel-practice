e12ce3fe283f9ed3cf875c5a88302000f5215788
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-04-03 22:38:20 +0900

    Add hello page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hello', function () {
+    return '<html><body><h1>Hello, World!</h1></body></html>';
+});
